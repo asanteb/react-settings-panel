1eb1fd9140757821d8aa5c2ce36ef11eea9129b7
asante.buil <asante.buil@tccd.edu>
2017-11-10 11:39:05 -0600

    implemented fontawesome to replace default uikit icon library

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import FontAwesome from 'react-fontawesome'
 import propTypes from 'prop-types'
 
 const styles = {
@@ -24,7 +25,15 @@ class Settings extends Component {
 			<div className='settings-layout'>
 				<nav className="uk-navbar uk-navbar-container uk-margin">
 					<div className="uk-navbar-left">
-						<a className="uk-navbar-toggle" data-uk-navbar-toggle-icon href="#"></a>
+						<a className="uk-navbar-toggle" href="#">
+								<FontAwesome
+								className='super-crazy-colors'
+								name='rocket'
+								size='2x'
+								spin
+								style={{ textShadow: '0 1px 0 rgba(0, 0, 0, 0.1)' }}
+							/>
+						</a>
 					</div>
 				</nav>
 				LoLOLolOl
