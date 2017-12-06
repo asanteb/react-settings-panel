a6a0dbca9c848f076822374695101147c9637c08
asantebuil <asante.buil@tccd.edu>
2017-12-05 23:20:38 -0600

    removed uk-margin error

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -53,7 +53,7 @@ class Settings extends Component {
 		let Groups = []
 		let SingleUnknownProp = null
 		let Submit = (
-			<p uk-margin='' style={{textAlign: 'right', padding: '1.5em'}}>
+			<p className='uk-margin' style={{textAlign: 'right', padding: '1.5em'}}>
 				<button onClick={this.clearData} style={{backgroundColor: 'white'}} className="uk-button uk-button-default">Cancel</button>
 				<button onClick={this.submitData} className="uk-button uk-button-primary">Submit</button>
 			</p>
