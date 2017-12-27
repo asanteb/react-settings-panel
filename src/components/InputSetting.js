8d40b16e54810846bac537d18407d3c6e145dd35
pedall <peterdodell@icloud.com>
2017-12-27 23:41:51 +0700

    initialValue for Input and Switch

--- src/components/InputSetting.js
+++ src/components/InputSetting.js
@@ -1,37 +1,43 @@
 import React, { Component } from 'react'
 import Switch from 'react-switch'
 import propTypes from 'prop-types'
+import { observer } from 'mobx-react'
 
 const styles = {
 
 }
 
+@observer
 class InputSetting extends Component {
 	static displayName = "INPUT_SETTING"
 	constructor() {
-    super()
-    this.state = {
+	super();
+	this.state = {
 			checked: false,
 			header: '',
-			value:''
+			value: ''
 		}
 	}
-	
+
+
 	componentDidMount () {
 		// this.initProps()
+		this.initialValue = this.props.initialValue ? this.props.initialValue : null;
+		if (this.initialValue) this.setState({ value: this.initialValue });
 	}
 
 
   handleChange = (e) => {
-		this.props.onChange(e.target.value)
-		this.setState({value: e.target.value})
+		if (this.props.onChange) this.props.onChange(this.props.store.settingsData);
+		this.props.store.settingsData[this.props.name] = e.target.value;
+		this.setState({value: e.target.value});
   }
 
   render() {
-		const title = this.props.title ? this.props.title : null
-		const description = this.props.description ? this.props.description : null
+		const title = this.props.title ? this.props.title : null;
+		const description = this.props.description ? this.props.description : null;
 
-    return (
+	return (
 			<div>
 				<dl className="uk-description-list uk-description-list-divider">
 					<dt>{this.props.title}</dt>
@@ -48,15 +54,17 @@ class InputSetting extends Component {
 					<hr/>
 				</dl>
 			</div>
-    )
+	)
   }
 }
 
 InputSetting.propTypes = {
-    onValue: propTypes.func,
+	onValue: propTypes.func,
 		onSubmit: propTypes.func,
 		title: propTypes.string,
-		description: propTypes.string
-}
+		description: propTypes.string,
+		name: propTypes.string.isRequired,
+		initialValue: propTypes.string
+};
 
 export default InputSetting
