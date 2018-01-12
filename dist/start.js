a54b7dc5d98058410a04de24e24538c8099f2644
pedall <peterdodell@icloud.com>
2018-01-12 12:59:12 +0100

    update to UIKit beta v36

--- dist/start.js
+++ dist/start.js
@@ -16,6 +16,10 @@ var _App = require('./App');
 
 var _App2 = _interopRequireDefault(_App);
 
+require('./assets/css/uikit.min.css');
+
+require('./assets/css/uikit.g4m3r-theme.min.css');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 //UIkit.use(Icons);
--- src/start.js
+++ src/start.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 import mobX from './mobx/store.js';
 import App from './App';
 
+import './assets/css/uikit.min.css';
+import './assets/css/uikit.g4m3r-theme.min.css';
+
 //UIkit.use(Icons);
 
 ReactDOM.render((
