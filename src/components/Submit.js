34dcb578dba10f725b5cbcbde7526b4828c76ceb
pedall <peterdodell@icloud.com>
2018-01-20 00:47:06 +0100

    make use of button optional

--- src/components/Submit.js
+++ src/components/Submit.js
@@ -1,26 +1,34 @@
 import React, { Component } from 'react';
-import FontAwesome from 'react-fontawesome'
-import propTypes from 'prop-types'
-import { observer } from 'mobx-react'
-const styles = {
+import FontAwesome from 'react-fontawesome';
+import propTypes from 'prop-types';
+import { observer } from 'mobx-react';
 
-}
+const styles = {};
+
 @observer
 class Submit extends Component {
-	static displayName = "SUBMIT"
-	constructor() {
+  static displayName = "SUBMIT";
+
+  constructor() {
     super();
     this.state = {
       styles: styles
-    }
+    };
   }
+
   render() {
     return (
-			<p className='uk-margin' style={{textAlign: 'right', padding: '1.5em'}}>
-        <button style={{backgroundColor: 'white'}} className="uk-button uk-button-default">Default</button>
-        <button className="uk-button uk-button-primary">Primary</button>
+      <p className='uk-margin' style={{ textAlign: 'right', padding: '1.5em' }}>
+        <button onClick={this.props.clear} style={{ backgroundColor: 'white' }} className="uk-button uk-button-default">Cancel</button>
+        <button onClick={this.props.submit} className="uk-button uk-button-secondary">Submit</button>
       </p>
-    )
+    );
   }
 }
-export default Submit
+
+Submit.propTypes = {
+  clear: propTypes.func,
+  submit: propTypes.func,
+};
+
+export default Submit;
