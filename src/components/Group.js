93434233d7d72f85707a8b107265fb02b5f0d11d
pedall <peterdodell@icloud.com>
2018-01-21 02:31:13 +0100

    add width prop to Group and Settings components too

--- src/components/Group.js
+++ src/components/Group.js
@@ -1,36 +1,44 @@
 import React, { Component } from 'react';
-import propTypes from 'prop-types'
-import md5 from 'md5'
-import { observer } from 'mobx-react'
+import propTypes from 'prop-types';
+import md5 from 'md5';
+import { observer } from 'mobx-react';
 
-const styles = {
+const styles = {};
 
-}
 @observer
 class Group extends Component {
-	static displayName = "GROUP"
-	constructor() {
+  static displayName = "GROUP";
+
+  constructor() {
     super();
     this.state = {
       styles: styles
-    }
+    };
   }
+
   render() {
-		let Children = []
-		if (this.props.children.forEach){
-			this.props.children.forEach((c, i) => {
-				const child = React.cloneElement(c, {
-					store: this.props.store,
-					key: md5(`group${i}`)
-				})
-				Children.push(child)
-			})
-		}
+    const width = typeof this.props.width === 'string' && this.props.width.includes('uk-width') ? this.props.width : "";
+    let Children = [];
+    if (this.props.children.forEach) {
+      this.props.children.forEach((c, i) => {
+        const child = React.cloneElement(c, {
+          store: this.props.store,
+          key: md5(`group${i}`)
+        });
+        Children.push(child);
+      });
+    }
     return (
-			<div className='uk-card uk-card-default uk-card-body'>
-					{Children.map(Child => Child)}
-			</div>
-    )
+      <div className={`uk-card uk-card-default uk-card-body ${width}`}>
+        {Children.map(Child => Child)}
+      </div>
+    );
   }
 }
+
+Group.propTypes = {
+  width: propTypes.string
+};
+
+
 export default Group;
