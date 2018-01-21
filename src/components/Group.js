ba6d7589a76fbd680733c559f58969f95a5bee89
pedall <peterdodell@icloud.com>
2018-01-21 02:16:17 +0100

    only try to render multiple Children if there are multiple children of Group

--- src/components/Group.js
+++ src/components/Group.js
@@ -1,36 +1,43 @@
 import React, { Component } from 'react';
-import propTypes from 'prop-types'
-import md5 from 'md5'
-import { observer } from 'mobx-react'
+import propTypes from 'prop-types';
+import md5 from 'md5';
+import { observer } from 'mobx-react';
 
-const styles = {
+const styles = {};
 
-}
 @observer
 class Group extends Component {
-	static displayName = "GROUP"
-	constructor() {
+  static displayName = "GROUP";
+
+  constructor() {
     super();
     this.state = {
       styles: styles
-    }
+    };
   }
+
   render() {
-		let Children = []
-		if (this.props.children.forEach){
-			this.props.children.forEach((c, i) => {
-				const child = React.cloneElement(c, {
-					store: this.props.store,
-					key: md5(`group${i}`)
-				})
-				Children.push(child)
-			})
-		}
+    let Children = [];
+    if (this.props.children && this.props.children.forEach) {
+      this.props.children.forEach((c, i) => {
+        const child = React.cloneElement(c, {
+          store: this.props.store,
+          key: md5(`group${i}`)
+        });
+        Children.push(child);
+      });
+    } else if (this.props.children) {
+      const child = React.cloneElement(this.props.children, {
+        store: this.props.store
+      });
+      Children.push(child);
+    }
     return (
-			<div className='uk-card uk-card-default uk-card-body'>
-					{Children.map(Child => Child)}
-			</div>
-    )
+      <div className='uk-card uk-card-default uk-card-body'>
+        {Children.length ? Children.map(Child => Child) : null}
+      </div>
+    );
   }
 }
+
 export default Group;
