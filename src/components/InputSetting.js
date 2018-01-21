2c9b824a9a74f5a0daeb7594044e57a8225ef334
pedall <peterdodell@icloud.com>
2018-01-21 02:08:24 +0100

    InputSetting, Textarea, SelectionSetting with optional width property using original uikit width classes (uk-width-*)

--- src/components/InputSetting.js
+++ src/components/InputSetting.js
@@ -1,66 +1,66 @@
-import React, { Component } from 'react'
-import Switch from 'react-switch'
-import propTypes from 'prop-types'
-import { observer } from 'mobx-react'
+import React, { Component } from 'react';
+import Switch from 'react-switch';
+import propTypes from 'prop-types';
+import { observer } from 'mobx-react';
 
-const styles = {
-
-}
+const styles = {};
 
 @observer
 class InputSetting extends Component {
-	static displayName = "INPUT_SETTING"
-	constructor() {
-	super()
-	this.state = {
-			checked: false,
-			header: '',
-			value:''
-		}
-	}
+  static displayName = "INPUT_SETTING";
 
-	componentDidMount () {
-		// this.initProps()
-	}
+  constructor() {
+    super();
+    this.state = {
+      checked: false,
+      header: '',
+      value: ''
+    };
+  }
+
+  componentDidMount() {
+    // this.initProps()
+  }
 
 
   handleChange = (e) => {
-		if (this.props.onChange) this.props.onChange(this.props.store.settingsData)
-		this.props.store.settingsData[this.props.name] = e.target.value
-		this.setState({value: e.target.value})
-  }
+    if (this.props.onChange) this.props.onChange(this.props.store.settingsData);
+    this.props.store.settingsData[this.props.name] = e.target.value;
+    this.setState({ value: e.target.value });
+  };
 
   render() {
-		const title = this.props.title ? this.props.title : null
-		const description = this.props.description ? this.props.description : null
-
-	return (
-			<div>
-				<dl className="uk-description-list uk-description-list-divider">
-					<dt>{this.props.title}</dt>
-					<input
-						type='text'
-						className='uk-input'
-						value={this.state.value}
-						onChange={this.handleChange}
-						id="settings-input"
-					/>
-					<dd>
-						{description}
-					</dd>
-					<hr/>
-				</dl>
-			</div>
-	)
+    const title = this.props.title ? this.props.title : null;
+    const description = this.props.description ? this.props.description : null;
+    const width = typeof this.props.width === 'string' && this.props.width.includes('uk-width') ? this.props.width : "";
+    return (
+      <div>
+        <dl className="uk-description-list uk-description-list-divider">
+          <dt>{this.props.title}</dt>
+          <input
+            type='text'
+            className={`uk-input ${width}`}
+            value={this.state.value}
+            onChange={this.handleChange}
+            id="settings-input"
+          />
+          <dd>
+            {description}
+          </dd>
+          <hr/>
+        </dl>
+      </div>
+    );
   }
 }
 
 InputSetting.propTypes = {
-	onValue: propTypes.func,
-		onSubmit: propTypes.func,
-		title: propTypes.string,
-		description: propTypes.string,
-		name: propTypes.string.isRequired
-}
+  onValue: propTypes.func,
+  onSubmit: propTypes.func,
+  title: propTypes.string,
+  description: propTypes.string,
+  name: propTypes.string.isRequired,
+  width: propTypes.string
+};
 
-export default InputSetting
+export default InputSetting;
