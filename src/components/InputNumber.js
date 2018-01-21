88d613488c2e7c0387a9302a4ca8c6bc44d6cd3d
pedall <peterdodell@icloud.com>
2018-01-21 21:28:22 +0100

    Merge branch 'master' into initialValue

--- src/components/InputNumber.js
+++ src/components/InputNumber.js
@@ -1,78 +1,77 @@
-import React, { Component } from 'react'
-import Switch from 'react-switch'
-import propTypes from 'prop-types'
-import { observer } from 'mobx-react'
+import React, { Component } from 'react';
+import Switch from 'react-switch';
+import propTypes from 'prop-types';
+import { observer } from 'mobx-react';
 
-const styles = {
-
-}
+const styles = {};
 
 @observer
 class InputNumber extends Component {
-	static displayName = "INPUT_NUMBER"
-	constructor() {
-		super()
-		this.state = {
-				value:0
-		}
-	}
+  static displayName = "INPUT_NUMBER";
 
-	componentWillMount() {
-		this.props.store.settingsData[this.props.name] = this.state.value ;
-	}
+  constructor() {
+    super();
+    this.state = {
+      value: 0
+    };
+  }
+
+  componentWillMount() {
+    this.props.store.settingsData[this.props.name] = this.state.value;
+  }
 
 
   handleChange = (e) => {
-		if (this.props.onChange) this.props.onChange(this.props.store.settingsData)
-		this.props.store.settingsData[this.props.name] = parseFloat(e.target.value)
-		this.setState({value: parseFloat(e.target.value)})
-  }
+    if (this.props.onChange) this.props.onChange(this.props.store.settingsData);
+    this.props.store.settingsData[this.props.name] = parseFloat(e.target.value);
+    this.setState({ value: parseFloat(e.target.value) });
+  };
 
   render() {
 
-	return (
-			<div>
-				<dl className="uk-description-list uk-description-list-divider">
-					<dt>{this.props.title}</dt>
-					<input
-						type='number'
-						className='uk-input'
-						value={this.state.value}
-						onChange={this.handleChange}
-						min={this.props.min}
-						max={this.props.max}
-						step={this.props.step}
-						id="settings-input"
-					/>
-					<dd>
-						{this.props.description}
-					</dd>
-					<hr/>
-				</dl>
-			</div>
-	)
+    return (
+      <div>
+        <dl className="uk-description-list uk-description-list-divider">
+          <dt>{this.props.title}</dt>
+          <input
+            type='number'
+            className='uk-input'
+            value={this.state.value}
+            onChange={this.handleChange}
+            min={this.props.min}
+            max={this.props.max}
+            step={this.props.step}
+            id="settings-input"
+          />
+          <dd>
+            {this.props.description}
+          </dd>
+          <hr/>
+        </dl>
+      </div>
+    );
   }
 }
 
 
 InputNumber.defaultProps = {
-	min: 0,
-	max: 100,
-	step: 1,
-	title: null, 
-	description: null
+  min: 0,
+  max: 100,
+  step: 1,
+  title: null,
+  description: null
 };
 
 
 InputNumber.propTypes = {
-	onValue: propTypes.func,
-	onSubmit: propTypes.func,
-	title: propTypes.string,
-	description: propTypes.string,
-	min: propTypes.number,
-	max: propTypes.number,
-	step: propTypes.number,
-	name: propTypes.string.isRequired
-}
+  onValue: propTypes.func,
+  onSubmit: propTypes.func,
+  title: propTypes.string,
+  description: propTypes.string,
+  min: propTypes.number,
+  max: propTypes.number,
+  step: propTypes.number,
+  name: propTypes.string.isRequired
+};
 
-export default InputNumber
+export default InputNumber;
