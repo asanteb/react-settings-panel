bf78506774a598d4dd2d7e8aabd5c8b855c4298d
pedall <peterdodell@icloud.com>
2018-01-22 22:42:45 +0100

    also the built files

--- dist/components/InputNumber.js
+++ dist/components/InputNumber.js
@@ -4,6 +4,10 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _defineProperty2 = require('babel-runtime/helpers/defineProperty');
+
+var _defineProperty3 = _interopRequireDefault(_defineProperty2);
+
 var _getPrototypeOf = require('babel-runtime/core-js/object/get-prototype-of');
 
 var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
@@ -57,7 +61,7 @@ var InputNumber = (0, _mobxReact.observer)(_class = (_temp = _class2 = function 
       _this.props.store.settingsData[_this.props.name] = parseFloat(e.target.value);
       _this.setState({ value: parseFloat(e.target.value) }, function () {
         if (_this.props.hasOwnProperty("onChange") && !_this.props.store) {
-          _this.props.onChange(parseFloat(e.target.value));
+          _this.props.onChange((0, _defineProperty3.default)({}, _this.props.name, parseFloat(e.target.value)));
         }
       });
     };
--- dist/components/TextAreaSetting.js
+++ dist/components/TextAreaSetting.js
@@ -4,6 +4,10 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _defineProperty2 = require('babel-runtime/helpers/defineProperty');
+
+var _defineProperty3 = _interopRequireDefault(_defineProperty2);
+
 var _getPrototypeOf = require('babel-runtime/core-js/object/get-prototype-of');
 
 var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
@@ -57,7 +61,7 @@ var TextArea = (0, _mobxReact.observer)(_class = (_temp = _class2 = function (_C
       if (_this.props.store && _this.props.onChange) _this.props.onChange(_this.props.store.settingsData);
       _this.setState({ value: e.target.value }, function () {
         if (_this.props.hasOwnProperty("onChange") && !_this.props.store) {
-          _this.props.onChange(e.target.value);
+          _this.props.onChange((0, _defineProperty3.default)({}, _this.props.name, e.target.value));
         }
       });
     };
